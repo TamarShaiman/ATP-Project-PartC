8db9c01942ff7fd3392fa19e4509e5241ca2960a
TamarShaiman <75303340+TamarShaiman@users.noreply.github.com>
2021-05-30 16:37:01 +0300

    Part c Initial

--- src/Model/MyModel.java
+++ src/Model/MyModel.java
@@ -1,4 +1,23 @@
 package Model;
+import algorithms.mazeGenerators.*;
 
-public class MyModel implements IModel{
+import java.util.Arrays;
+
+public class MyModel {
+    /*public static void main(String[] args) {
+        AMazeGenerator generator = new MyMazeGenerator();
+        Maze maze = generator.generate(5, 5);
+        maze.print();*/
+        //System.out.println(Arrays.deepToString(maze));
+
+
+    public int[][] generateRandomMaze(int rows, int cols){
+        int[][] maze = new int[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                maze[i][j] = (int) Math.round(Math.random());
+            }
+        }
+        return maze;
+    }
 }
